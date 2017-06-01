7dbd98a3df1f755d5e626639f7435c6b35b1d5c9
Victor de Moustier <vmoustier@gmail.com>
2017-06-01 18:25:09 +0000

    add edit and signup form

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -6,10 +6,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to teh AlphaBlog #{@user.username}"
+      flash[:success] = "Welcome to the AlphaBlog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account #{@user.username} has been successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
   
